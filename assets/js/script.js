1b9bd4095cb24cb4fe3e5c5ef01e9c8d6e4213bf
saadahmad888 <saadbhatti888@gmail.com>
2024-02-26 13:11:42 +0500

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,23 @@ $(function() {
   // SmartWizard initialize
   $('#smartwizard').smartWizard();
 });
+
+
+
+// Show div on step 9
+$(document).ready(function(){
+  // Hide the info box initially
+  $("#step9-info-box").hide();
+
+  // Attach click event listener to radio buttons
+  $("input[name='how-long-shap-of-life']").click(function(){
+      // Check if any radio button is checked
+      if($(this).is(":checked")){
+          // Show the info box if a radio button is checked
+          $("#step9-info-box").show();
+      } else {
+          // Hide the info box if no radio button is checked
+          $("#step9-info-box").hide();
+      }
+  });
+});
